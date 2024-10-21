492fb096d24b8a93cd1c8c2a73212cbfd97fa850
kodxxl <xsrc@mail.ru>
2024-10-21 10:04:00 +0400

    This algorythm not pass by time

--- forcycle/forcycle_test.go
+++ forcycle/forcycle_test.go
@@ -2,6 +2,21 @@ package forcycle
 
 import "testing"
 
-func TestminSubArrayLen(t *testing.T) {
+func TestMinSubArrayLen(t *testing.T) {
+	tests := []struct {
+		target   int
+		nums     []int
+		expected int
+	}{
+		{7, []int{2, 3, 1, 2, 4, 3}, 2},
+		{4, []int{1, 4, 4}, 1},
+		{11, []int{1, 1, 1, 1, 1, 1, 1, 1}, 0},
+		{4, []int{2, 1, 2, 1}, 3},
+	}
 
+	for i, v := range tests {
+		if MinSubArrayLen(v.target, v.nums) != v.expected {
+			t.Errorf("failed %v", i)
+		}
+	}
 }
