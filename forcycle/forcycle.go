9514fc43138393fb1ad554f3e887405fd30d129a
kodxxl <xsrc@mail.ru>
2024-10-21 10:15:41 +0400

    Initial commit for Sliding windows algorythm

--- forcycle/forcycle.go
+++ forcycle/forcycle.go
@@ -1,26 +1,5 @@
 package forcycle
 
 func MinSubArrayLen(target int, nums []int) int {
-	numsLen := len(nums)
-	minSubArrayLen := numsLen + 1
-	for i := range nums {
-		sum := 0
-		subArrayLen := 0
-		found := false
-		for j := i; j < numsLen; j++ {
-			sum += nums[j]
-			subArrayLen++
-			if sum >= target {
-				found = true
-				break
-			}
-		}
-		if (subArrayLen < minSubArrayLen) && found {
-			minSubArrayLen = subArrayLen
-		}
-	}
-	if minSubArrayLen > numsLen {
-		minSubArrayLen = 0
-	}
-	return minSubArrayLen
+
 }
